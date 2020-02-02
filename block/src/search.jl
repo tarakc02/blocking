bec492356a324d1b4ab41f8d0a5b67a854efe07b
Tarak Shah <tarak@hrdag.org>
2020-02-02 10:16:56 -0800

    multi-thread the part where we search for rules

--- block/src/search.jl
+++ block/src/search.jl
@@ -5,25 +5,30 @@ using ..Conjunctions
 
 function add_greedy(conjunction)
     problem = conjunction.problem
-    current = conjunction
-    current_ratio = current.value / current.cost
-    for rule in conjunction.unselected
+    init_ratio = conjunction.value / conjunction.cost
+    current_ratios = [init_ratio for thread in 1:Threads.nthreads()]
+    current_conjs  = [conjunction for thread in 1:Threads.nthreads()]
+    all_rules = collect(conjunction.unselected)
+    Threads.@threads for rule in all_rules
         candidate = Conjunctions.and(conjunction, rule)
         candidate_ratio = candidate.value / candidate.cost
+        current_ratio = current_ratios[Threads.threadid()]
+        current_value = current_conjs[Threads.threadid()].value
         candidate_ratio < current_ratio && continue
         if candidate_ratio > current_ratio
-            current = candidate
-            current_ratio = candidate_ratio
+            current_conjs[Threads.threadid()] = candidate
+            current_ratios[Threads.threadid()] = candidate_ratio
             continue
         end
-
-        if candidate.value > current.value
-            current = candidate
-            current_ratio = candidate_ratio
+        if candidate.value > current_value
+            current_conjs[Threads.threadid()] = candidate
+            current_ratios[Threads.threadid()] = candidate_ratio
         end
     end
     # if we end up with a solution that is too expensive, just add more
     # conjuncts until we have something acceptable
+    maxval, maxindex = findmax(current_ratios)
+    current = current_conjs[maxindex]
     if current.cost > problem.budget && !isempty(current.unselected)
         current == conjunction && return current
         print("   ...recursing...\n")
